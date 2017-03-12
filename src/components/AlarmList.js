6a8f14216be1fdc5fc6e5cf157c1d1491bebdcf1
Pavel Zubkou <pavel.zubkou@gmail.com>
2017-03-13 00:56:42 +0300

    Check for case of no alarms

--- src/components/AlarmList.js
+++ src/components/AlarmList.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const AlarmList = ({ alarms = [] }) => {
+  if (alarms.length === 0) {
+    return <p>No alarms set</p>;
+  }
+
   return (
     <ul>
       {alarms.map(alarm => <li>{alarm}</li>)}
