49a0b5cabec4541a4295eb7feb514eeebb9c4b52
Pavel Zubkou <pavel.zubkou@gmail.com>
2017-03-14 23:29:49 +0300

    Use airbnb style and change code according to it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* eslint-env browser */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -5,5 +7,5 @@ import './index.css';
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
